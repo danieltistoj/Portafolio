fdbeba7ab89e66b29abcb5fe14fc83a6e512a857
danieltistoj <josetisrey@gmail.com>
2023-06-03 22:31:55 -0600

    envio de mensaje con correo electronico listo

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -51,3 +51,21 @@ titular.forEach((CadaTitular,i) =>{
         }
     })
 })
+/*Codigo envio de formulario */
+    const $form = document.querySelector("#form")
+    $form.addEventListener("submit",handleSubmit)
+    async function handleSubmit(event){
+        event.preventDefault()
+        const form = new FormData(this);
+        const response = await fetch(this.action, {
+            method:this.method,
+            body:form,
+            headers:{
+                'Accept': 'application/json'
+            }
+        })
+        if(response.ok) {
+            this.reset()
+            alert("Gracias por contactarme, te escribire pronto")
+        }
+    }
